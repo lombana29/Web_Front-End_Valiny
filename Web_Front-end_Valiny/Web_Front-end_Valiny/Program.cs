6568add3ad9e7d19a7340dd1c20ea25fdd696a3a
lombana29 <cristian.d.bernate05@gmail.com>
2023-12-13 01:13:20 -0500

    feat(front-end): Add Program Class,Interfaces and DTOs

--- Web_Front-end_Valiny/Web_Front-end_Valiny/Program.cs
+++ Web_Front-end_Valiny/Web_Front-end_Valiny/Program.cs
@@ -1,9 +1,20 @@
+using WebApplicationBilling.Repository;
+using WebApplicationBilling.Repository.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//Habilitar el cliente Http
+builder.Services.AddHttpClient();
+
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,6 +28,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+//Damos Soporte Cors
+app.UseCors(c => c
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
 
 app.UseAuthorization();
 
---
+++ Web_Front-end_Valiny/Web_Front-end_Valiny/Repository/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using Web_Front_end_Valiny.Repository.Interfaces;
+
+namespace Web_Front_end_Valiny.Repository.Interfaces
+{
+    public interface ICustomerRepository : IRepository<Models.DTO.CustomerDTO>
+    {
+    }
+}
